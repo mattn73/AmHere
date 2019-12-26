9ceeac92a98269360aae76f5e5d45dbc1607915e
Matthieu Conhyea <mattyconhyea@gmail.com>
2019-12-26 19:41:44 +0400

    Update git ignore and add twig to project with debug

--- public/index.php
+++ public/index.php
@@ -1,3 +1,30 @@
 <?php
 
-(require __DIR__ . '/../config/bootstrap.php')->run();
+use Slim\Views\Twig;
+use Psr\Container\ContainerInterface;
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Twig\Loader\FilesystemLoader;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$container = new Container();
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$container = $app->getContainer();
+
+$loader = new FilesystemLoader(__DIR__ . '/../templates');
+
+$container->set('view', function(ContainerInterface $container) use ($loader){
+    return new Twig($loader);
+});
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->get('view')->render($response, 'hello.twig', [
+        'name' => $args['name']
+    ]);
+});
+
+$app->run();
